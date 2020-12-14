60d16c05479a7e98580e673741d04fc941411fc7
Seniru <senirupasan@gmail.comm>
2020-12-14 23:59:15 +0530

    feat: Fail verification if member has verified
    - Fail the verification process if the member has verified already, ie
    has the verified role
    - Delete messages sent in #verify channel after a time out

--- src/bot.lua
+++ src/bot.lua
@@ -37,28 +37,39 @@ end
 
 cmds["verify"] = function(args, msg)
 
-    verificationKeys[msg.author.id] = { key = generateKey(10), toLink = args[2], attempts = 5 }
+    if msg.member:hasRole(config["role-verified"]) then
+        local m = msg.channel:send("You are verified already!")
+        return timer.setTimeout(1000 * 60 * 1, coroutine.wrap(function()
+            msg:delete()
+            m:delete()
+        end))
+    end
 
+    verificationKeys[msg.author.id] = { key = generateKey(10), toLink = args[2], attempts = 5 }
+    timer.setTimeout(1000 * 60 * 1, coroutine.wrap(function() msg:delete() end))
     local res, err = forum.createPrivateMessage(args[2], "AR Helpers verification", "Here's your verification key: " .. verificationKeys[msg.author.id].key)
 
     if not res then
         print(err)
         verificationKeys[msg.author.id] = nil
-        return msg.channel:send("We encountered an error while sending the DM... check if your username is correct and try again later!")
+        local m = msg.channel:send("We encountered an error while sending the DM... check if your username is correct and try again later!")
+        return timer.setTimeout(1000 * 60 * 1, coroutine.wrap(function() m:delete() end))
     end
 
 
-    msg.channel:send({
+    local e = msg.channel:send({
         embed = {
             title = "Verification",
             description = "We sent a DM to your forum to confirm it's you\n\n[Open](https://atelier801.com/conversation?" .. res.raw_data .. ")"
         }
     })
+    timer.setTimeout(1000 * 60 * 5, coroutine.wrap(function() e:delete() end))
 
-    timer.setTimeout(1000 * 60 * 1, coroutine.wrap(function() -- expire the key in 5 minutes
+    timer.setTimeout(1000 * 60 * 5, coroutine.wrap(function() -- expire the key in 5 minutes
         if not verificationKeys[msg.author.id] then return end
         verificationKeys[msg.author.id] = nil
-        msg.channel:send("<@" .. msg.author.id .. "> Verification process timed out! Please do !verify again")
+        local m = msg.channel:send("<@" .. msg.author.id .. "> Verification process timed out! Please do !verify again")
+        timer.setTimeout(1000 * 60 * 5, coroutine.wrap(function() m:delete() end))
     end))
 
 end
@@ -82,15 +93,19 @@ discord:on("messageCreate", function(msg)
         if keys then
             -- checking if the given key matches with the generated key
             if keys.key == msg.content then
-                msg.channel:send("Linking you with " .. keys.toLink)
+                local m = msg.channel:send("Linking you with " .. keys.toLink)
+                timer.setTimeout(1000 * 60 * 1, coroutine.wrap(function() m:delete() end))
                 verificationKeys[msg.author.id] = nil
+                msg.member:addRole(config["role-verified"])
                 msg.member:setNickname(keys.toLink)
             else
                 if keys.attempts < 1 then
                     verificationKeys[msg.author.id] = nil
-                    return msg.channel:send("Verification failed!!! Please try again later")
+                    local m = msg.channel:send("Verification failed!!! Please try again later")
+                    return timer.setTimeout(1000 * 60 * 1, coroutine.wrap(function() m:delete() end))
                 end
-                msg.channel:send("Wrong key! ** " .. keys.attempts .. "** attempts left!")
+                local m = msg.channel:send("Wrong key! **" .. keys.attempts .. "** attempts left!")
+                timer.setTimeout(1000 * 60 * 1, coroutine.wrap(function() m:delete() end))
                 verificationKeys[msg.author.id].attempts = keys.attempts - 1
             end
             return msg:delete()
--- src/config.lua
+++ src/config.lua
@@ -2,8 +2,10 @@ local config = {}
 
 if os.getenv("ENV") == "test" then
     config["verification-channel"] = "787593880069079071"
+    config["role-verified"] = "715186122636001300"
 else
     config["verification-channel"] = "787286149755568139"
+    config["role-verified"] = "787027339900092447"
 end
 
 return config
