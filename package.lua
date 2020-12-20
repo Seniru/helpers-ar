d78cc533cb504b531e32693472544a00a4ca63ca
Seniru <senirupasan@gmail.comm>
2020-12-20 12:26:36 +0530

    chore: Few host configurations

---
+++ package.lua
@@ -0,0 +1,10 @@
+return {
+    name = "Seniru/helpers-ar",
+    version = "1.0.0",
+    description = "A discord bot for Transformie AR Helper teams",
+    dependencies = {
+      "luvit/luvit"
+      --"SinisterRectus/discordia",
+      --"Lautenschlager-id/fromage"
+    },
+  }
