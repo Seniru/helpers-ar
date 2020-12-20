2d5d14d827a84fb2bccb0b7316de555639ce15b4
Seniru <senirupasan@gmail.comm>
2020-12-20 12:16:49 +0530

    lang: Change EN to AR

--- src/bot.lua
+++ src/bot.lua
@@ -12,7 +12,6 @@ local discord = discordia.Client()
 -- [[ configuration stuff ]]
 local PREFIX = "!"
 
-
 -- [[ verification system ]]
 local verificationKeys = {}
 local abc = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
@@ -31,48 +30,68 @@ end
 -- [[ commands ]]
 local cmds = {}
 
-cmds["ping"] = function(args, msg)
-    msg.channel:send("Pong!")
-end
+cmds["ping"] = {
+    isForumAction = false,
+    f = function(args, msg)
+        msg.channel:send("Pong!")
+    end
+}
+cmds["verify"] = {
+    isForumAction = true,
+    f = function(args, msg)
 
-cmds["verify"] = function(args, msg)
+        if not args[2] then
+            local m = msg.channel:send("Please enter a valid username")
+            return timer.setTimeout(1000 * 60 * 1, coroutine.wrap(function()
+                msg:delete()
+                m:delete()
+            end))
+        end
 
-    if msg.member:hasRole(config["role-verified"]) then
-        local m = msg.channel:send("You are verified already!")
-        return timer.setTimeout(1000 * 60 * 1, coroutine.wrap(function()
-            msg:delete()
-            m:delete()
+        if msg.member:hasRole(config["role-verified"]) then
+            local m = msg.channel:send("You are verified already!")
+            return timer.setTimeout(1000 * 60 * 1, coroutine.wrap(function()
+                msg:delete()
+                m:delete()
+            end))
+        end
+
+        verificationKeys[msg.author.id] = { key = generateKey(10), toLink = args[2], attempts = 5 }
+        timer.setTimeout(1000 * 60 * 1, coroutine.wrap(function() msg:delete() end))
+
+        local res, err = forum.createPrivateMessage(args[2], "AR Helpers verification",
+        ([[ [p=center][img]https://i.imgur.com/w16ABsN.png[/img][/p]
+        مرحبا بك [color=#FEB1FC][b]%s[/b][/color]
+        
+        شكرا على إنضمامك[b] [color=#2E72CB]لمخدم فريق المساعدين[/color][/b] , لإتمام عملية التسجيل في المخدم الخاص بنا انسخ الكود التالي وقم بإرساله عن طريق القناة الموجودة في المخدم الخاص بنا [b][color=#2E72CB]#[/color][color=#2E72CB]verification[/color][/b]
+        [hr]
+        [b][p=center]الكود الخاص بك هو : [color=#FEB1FC]%s[/color] [/p][/b]
+        [hr] ]]):format(verificationKeys[msg.author.id].toLink, verificationKeys[msg.author.id].key)
+        )
+
+        if not res then
+            print(err)
+            verificationKeys[msg.author.id] = nil
+            local m = msg.channel:send("لقد واجهنا خطأ اثاء ارسال رساله خاصه للتحقق من اسم المستخدم الخاص بك حاول مره اخرى لاحقاً!")
+            return timer.setTimeout(1000 * 60 * 1, coroutine.wrap(function() m:delete() end))
+        end
+
+        local e = msg.channel:send({
+            embed = {
+                title = "Verification",
+                description = "لقد ارسلنا رساله خاصه في المنتدى الخاص بك للتأكيد من هويتك \n\n [مفتوح](https://atelier801.com/conversation?" .. res.raw_data .. ")"
+            }
+        })
+        timer.setTimeout(1000 * 60 * 5, coroutine.wrap(function() e:delete() end))
+        timer.setTimeout(1000 * 60 * 5, coroutine.wrap(function() -- expire the key in 5 minutes
+            if not verificationKeys[msg.author.id] then return end
+            verificationKeys[msg.author.id] = nil
+            local m = msg.channel:send("<@" .. msg.author.id .. "> فشل التحقق!!! يرجى المحاوله مره اخرى")
+            timer.setTimeout(1000 * 60 * 5, coroutine.wrap(function() m:delete() end))
         end))
     end
 
-    verificationKeys[msg.author.id] = { key = generateKey(10), toLink = args[2], attempts = 5 }
-    timer.setTimeout(1000 * 60 * 1, coroutine.wrap(function() msg:delete() end))
-    local res, err = forum.createPrivateMessage(args[2], "AR Helpers verification", "Here's your verification key: " .. verificationKeys[msg.author.id].key)
-
-    if not res then
-        print(err)
-        verificationKeys[msg.author.id] = nil
-        local m = msg.channel:send("We encountered an error while sending the DM... check if your username is correct and try again later!")
-        return timer.setTimeout(1000 * 60 * 1, coroutine.wrap(function() m:delete() end))
-    end
-
-
-    local e = msg.channel:send({
-        embed = {
-            title = "Verification",
-            description = "We sent a DM to your forum to confirm it's you\n\n[Open](https://atelier801.com/conversation?" .. res.raw_data .. ")"
-        }
-    })
-    timer.setTimeout(1000 * 60 * 5, coroutine.wrap(function() e:delete() end))
-
-    timer.setTimeout(1000 * 60 * 5, coroutine.wrap(function() -- expire the key in 5 minutes
-        if not verificationKeys[msg.author.id] then return end
-        verificationKeys[msg.author.id] = nil
-        local m = msg.channel:send("<@" .. msg.author.id .. "> Verification process timed out! Please do !verify again")
-        timer.setTimeout(1000 * 60 * 5, coroutine.wrap(function() m:delete() end))
-    end))
-
-end
+}
 
 discord:on("ready", function()
     print('[DISCORD] Logged in as '.. discord.user.username)
@@ -101,10 +120,10 @@ discord:on("messageCreate", function(msg)
             else
                 if keys.attempts < 1 then
                     verificationKeys[msg.author.id] = nil
-                    local m = msg.channel:send("Verification failed!!! Please try again later")
+                    local m = msg.channel:send("فشل التحقق!!! يرجى المحاوله مره اخرى")
                     return timer.setTimeout(1000 * 60 * 1, coroutine.wrap(function() m:delete() end))
                 end
-                local m = msg.channel:send("Wrong key! **" .. keys.attempts .. "** attempts left!")
+                local m = msg.channel:send(("رمز تحقق خاطئ!%s محاولات باقيه"):format(keys.attempts))
                 timer.setTimeout(1000 * 60 * 1, coroutine.wrap(function() m:delete() end))
                 verificationKeys[msg.author.id].attempts = keys.attempts - 1
             end
@@ -115,12 +134,15 @@ discord:on("messageCreate", function(msg)
     -- general commands
     if msg.content:sub(1, 1) ~= PREFIX then return end
     local args = utils.string_split(msg.content:sub(2), " ")
-    if cmds[args[1]] then
-        coroutine.wrap(function() cmds[args[1]](args, msg) end)()
+    local cmd = cmds[args[1]]
+    if cmd then
+        if cmd.isForumAction then
+            if not (forum.isConnected() or forum.isConnectionAlive()) then
+                forum.connect(os.getenv("USERNAME"), os.getenv("PASSWORD"))
+            end
+        end
+        coroutine.wrap(function() cmd.f(args, msg) end)()
     end
 end)
-
-
-
 
 discord:run("Bot " .. os.getenv("DISCORD"))
